b27496e13397326b58a0114f3f21a262a977b965
Rodrigo Villalba <rodris.jvo@gmail.com>
2017-07-26 15:39:59 -0400

    Add broken test

---
+++ initial/src/test/java/py/com/sodep/BrokenTest.java
@@ -0,0 +1,17 @@
+package py.com.sodep;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by rodrigovillalba on 7/26/17.
+ */
+
+public class BrokenTest {
+
+    @Test
+    public void test() {
+        Assert.fail("Force broken test");
+    }
+}
